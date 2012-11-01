d7218db384542ea6672e148b016c5b5f3e104feb
Jeff Buck <jeffreybuck@mac.com>
2012-11-01 00:57:14 -0400

    Added better jitter

--- README.md
+++ README.md
@@ -9,7 +9,8 @@ dotchartdf
 
 **Usage**
 
-`dotchartdf(data, xvar, yvar, title, alpha=0.05, log=FALSE)`
+	dotchartdf(data, xvar, yvar, title, alpha=0.05, dividers=TRUE,
+	           log=FALSE, dotspacing=0.07, legend=TRUE)
 
 
 **Description**
@@ -19,9 +20,13 @@ Creates a dotchart from a dataframe.
 `xvar` is the name of the dataframe column that will be used as the factor.  
 `yvar` is the name of the dataframe column that will be plotted.  
 `title` will be used as the title of the plot.  
-`alpha` sets the confidence interval. The default value is 0.05.  
+`alpha` sets the confidence interval. The default value is 0.05. If alpha is set to 0,
+	no confidence intervals or mean/median lines will be drawn.  
+`dividers` is a flag that will draw vertical dividers between factors if set to TRUE. Default is TRUE. 
 `log` is a flag that will take the log of the yvar values if set to TRUE. Default is FALSE.  
-
+`dotspacing` sets the horizontal spacing between dots with the same value in a factor.
+	Setting dotspacing is useful when there are lots of dots or very few factors.  
+`legend` is a flag that will draw a legend on the plot if set to TRUE. Default is TRUE.
 
 **Sample usage**
 
@@ -31,15 +36,17 @@ Creates a dotchart from a dataframe.
 
 	data(airquality)
 	dotchartdf(airquality, "Month", "Temp", "Temperature by Month")
-	dotchartdf(airquality, "Month", "Ozone", "Ozone by Month", 0.01, log=TRUE)
+
+	# Take the log of the Ozone values to try to get closer to a normal distribution
+	dotchartdf(airquality, "Month", "Ozone", "Ozone by Month", alpha=0.01, log=TRUE)
 
 	data(iris)
-	dotchartdf(iris, "Species", "Sepal.Length", "Sepal Length by Species", 0.05)
+	dotchartdf(iris, "Species", "Sepal.Length", "Sepal Length by Species", dotspacing=0.04)
 
 	# Not the greatest dataset to use, but a simple example of multiple dotcharts
 	par(mfrow=c(2,2))
 	for (param in c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")) {
-		dotchartdf(iris, "Species", param, paste(param,"by Species"), 0.05)  
+		dotchartdf(iris, "Species", param, paste(param,"by Species"), dotspacing=0.04, legend=FALSE)  
 	}
 	par(mfrow=c(1,1))
 
@@ -55,8 +62,7 @@ Jeff Buck.
 
 dotchartdf is based on sample code by [Brian Caffo](http://www.bcaffo.com) from his excellent 
 [Mathematical Biostatistics Boot Camp](https://www.coursera.org/course/biostats) course.
-Highly recommended.
-
+Highly recommended. Damn the mosquitos.
 
 
 
--- dotchartdf.r
+++ dotchartdf.r
@@ -1,54 +1,108 @@
-# Function for plotting dotcharts with data frames
-# 
-# 
-# dotchartdf(InsectSprays, "spray", "count", "Insect Deaths by Insecticide Type", 0.05, log=FALSE)
-dotchartdf <- function(data, xvar, yvar, title="", alpha=0.05, dividers=TRUE, log=FALSE) {
+## Function for plotting dotcharts from data frames:
+## 
+## dotchartdf <- function(data, xvar, yvar, title="", alpha=0.05, dividers=TRUE,
+##                        log=FALSE, dotspacing=0.07, legend=TRUE)
+##
+##  data - a dataframe
+##  xvar - name of the factor column in the dataframe
+##  yvar - name of the numeric column in the dataframe to plot
+##  title - chart title
+##  alpha - confidence interval
+##  dividers - whether to draw vertical dividers between factors
+##  log - whether to first take the log of yvar values before plotting
+##  dotspacing - fine-tune the space between dots in the plot
+##               useful when there are lots of dots or very few factors
+##  legend - whether to draw a legend on the plot
+##
+## Sample usage:
+##
+## data(InsectSprays)
+## dotchartdf(InsectSprays, "spray", "count", "Insect Deaths by Insecticide Type")
+##
+## data(airquality)
+## dotchartdf(airquality, "Month", "Temp", "Temperature by Month")
+## dotchartdf(airquality, "Month", "Ozone", "log(Ozone) by Month", alpha=0.05, log=TRUE)
+## 
+## data(iris)
+## dotchartdf(iris, "Species", "Sepal.Length", "Sepal Length by Species", alpha=0.01, dotspacing=0.04)
+## 
+## par(mfrow=c(2,2))
+## for (param in c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")) {
+## dotchartdf(iris, "Species", param, paste(param,"by Species"))
+## }
+## par(mfrow=c(1,1))
+
+
+## dotplotjitter - helper function to space and center the dots
+dotplotjitter <- function(i, data, dotspacing = 0.05) {
+  idx <- 1
+  x <- rep(i, length(data))
+  y <- rep(-1, length(data))
+  tdata <- table(data)
+
+  for (t in 1:length(tdata)) {
+    # centerline is i
+    value <- as.numeric(names(tdata[t]))
+    
+    startoff = i - dotspacing * (tdata[t]-1)/2
+    
+    for (j in 1:tdata[t]) {
+      y[idx] <- value
+      x[idx] <- startoff + dotspacing * (j - 1)
+      idx <- idx + 1
+    }
+  }
+  
+  cbind(x, y)
+}
+
+
+dotchartdf <- function(data, xvar, yvar, title="", alpha=0.05, dividers=TRUE,
+                       log=FALSE, dotspacing=0.07, legend=TRUE) {
   d <- data[!is.na(data[,yvar]),]  # remove NA's
   if (log) {
     d[,yvar] <- log10(d[,yvar])
+    ylab <- paste("log(", yvar, ")", sep="")
   }
-  
+  else
+  {
+    ylab <- yvar
+  }
+
+  if (alpha == 0.0) { legend = FALSE }
+
   splitdata <- split(d, d[,xvar], drop=TRUE)  # factor by xvar
   plot(NULL, xlim=c(0.5, length(splitdata) + 0.5), ylim=range(d[,yvar]),
-       xlab=xvar, ylab=yvar, main=title,
-       xaxt='n')
+       xlab=xvar, ylab=ylab, main=title, xaxt='n')
   axis(side=1, at=1:length(splitdata), labels=names(splitdata))  # use factor names
-  legend("bottomright", legend=c("mean", "median"), col=c("blue", "orange"), lty=2)
+  
+  if (legend) {
+    legend("bottomright", legend=c("mean", "median", paste("alpha:",alpha)), 
+           col=c("blue", "orange", "blue"), lty=c(2, 2, 1), cex=0.7)
+  }
   if (dividers) {
-    sapply(1:(length(splitdata)-1), function(x) { abline(v=x+0.5, col="gray") })  # dividers    
+    sapply(1:(length(splitdata)-1), function(x) { abline(v=x+0.5, col="gray") })
   }
   
   for (i in 1:length(splitdata)) {
     s <- splitdata[[i]][,yvar]  # get the ith factor
     ci <- mean(s) + c(-1, 1) * qnorm(1-alpha/2) * sd(s) / sqrt(length(s))
-    normals <- (s > ci[1] & s < ci[2])  # bool vector for vals within ci
-    points(jitter(rep(i, length(s[normals])), amount=0.1), s[normals],
+    
+    plotdata <- dotplotjitter(i, s, dotspacing)
+    normals <- (plotdata[,2] > ci[1] & plotdata[,2] < ci[2])
+    points(plotdata[,1][normals], plotdata[,2][normals],
            pch=21, bg="lightblue")
-    points(jitter(rep(i, length(s[!normals])), amount=0.1), s[!normals],
-           pch=21, bg="red")  # possible outliers
+    points(plotdata[,1][!normals], plotdata[,2][!normals],
+           pch=21, bg="red")
 
-    xrange <- c(-0.3, 0.3)  # width of our annotation lines and box
-    rect(i + xrange[1], ci[1], i + xrange[2], ci[2], lwd=1, border="blue")  # ci box
-    lines(i + xrange, rep(mean(s), 2), lwd=1, col="blue", lty=2)  # mean
-    lines(i + xrange, rep(median(s), 2), lwd=1, col="orange", lty=2)  # median
+    if (alpha != 0.0) {
+      xrange <- c(-0.3, 0.3)  # width of our annotation lines and box
+      rect(i + xrange[1], ci[1], i + xrange[2], ci[2], lwd=1, border="blue")  # ci box
+      lines(i + xrange, rep(mean(s), 2), lwd=1, col="blue", lty=2)  # mean
+      lines(i + xrange, rep(median(s), 2), lwd=1, col="orange", lty=2)  # median
+    }
   }
 }
 
-# Sample usage:
-#
-# data(InsectSprays)
-# data(airquality)
-# data(iris)
-# 
-# dotchartdf(InsectSprays, "spray", "count", "Insect Deaths by Insecticide Type", 0.05, log=FALSE)
-# dotchartdf(airquality, "Month", "Temp", "Temperature by Month")
-# dotchartdf(airquality, "Month", "Ozone", "Ozone by Month", 0.05, log=TRUE)
-# dotchartdf(iris, "Species", "Sepal.Length", "Sepal Length by Species", 0.05)
-#
-# Not the greatest dataset to use, but a simple example of multiple dotcharts
-# par(mfrow=c(2,2))
-# for (param in c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")) {
-#   dotchartdf(iris, "Species", param, paste(param,"by Species"), 0.05)  
-# }
-# par(mfrow=c(1,1))
+
 
